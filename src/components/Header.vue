be099a5f5719a5b95a0b50649b3c93b1e271bc58
Valkyrux <philadephos.granata@gmail.com>
2022-01-24 15:27:21 +0100

    require

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <nav class="d-flex justify-content-between">
+            <div class="nav-link">lang</div>
+            <div class="justify-content-between">
+                <ul class="nav">
+                    <li class="nav-item" v-for="(element,index) in menuVoices"
+                    :key="'menu-voice' + index">
+                        <a class="nav-link active" href="#">Active</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div></div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      menuVoices: null,
+    };
+  },
+  create() {
+    this.menuVoices = require('../menuLinks.json');
+  },
+};
+</script>
+
+<style>
+
+</style>
