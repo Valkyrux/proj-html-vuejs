875f2b7cd46b3fdf3b797789907ed8552149a1eb
Valkyrux <philadephos.granata@gmail.com>
2022-01-24 16:34:56 +0100

    header-top

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,49 @@
 <template>
     <header>
-        <nav class="d-flex justify-content-between">
-            <div class="nav-link">lang</div>
-            <div class="justify-content-between">
+        <nav class="d-flex justify-content-between ms_header-top nav">
+            <div class="nav-link">
+              <a href="#">English <font-awesome-icon :icon="[ 'fas', 'chevron-down' ]" /></a>
+            </div>
+            <div class="d-flex justify-content-between align-items-center w-50">
                 <ul class="nav">
                     <li class="nav-item" v-for="(element,index) in menuVoices"
                     :key="'menu-voice' + index">
-                        <a class="nav-link active" href="#">Active</a>
+                        <a class="nav-link" :href="element.href">{{ element.name }}</a>
                     </li>
+                </ul>
+                <ul class="nav fs-6">
+                  <li class="nav-item p-2">
+                    <a href="#">
+                      <font-awesome-icon :icon="[ 'fab', 'twitter' ]" /></a>
+                  </li>
+                  <li class="nav-item p-2">
+                    <a href="#">
+                      <font-awesome-icon :icon="[ 'fab', 'instagram' ]" /></a>
+                  </li>
+                  <li class="nav-item p-2">
+                    <a href="#">
+                      <font-awesome-icon :icon="[ 'fab', 'behance' ]" /></a>
+                  </li>
+                  <li class="nav-item p-2">
+                    <a href="#">
+                      <font-awesome-icon :icon="[ 'fab', 'dribbble' ]" /></a>
+                  </li>
+                  <li class="nav-item p-2">
+                    <a href="#">
+                      <font-awesome-icon :icon="[ 'fab', 'flickr' ]" /></a>
+                  </li>
+                  <li class="nav-item p-2">
+                    <a href="#">
+                      <font-awesome-icon :icon="[ 'fab', 'git' ]" /></a>
+                  </li>
+                  <li class="nav-item p-2">
+                    <a href="#">
+                      <font-awesome-icon :icon="[ 'fab', 'linkedin' ]" /></a>
+                  </li>
+                  <li class="nav-item p-2">
+                    <a href="#">
+                      <font-awesome-icon :icon="[ 'fab', 'pinterest' ]" /></a>
+                  </li>
                 </ul>
             </div>
         </nav>
@@ -23,12 +59,23 @@ export default {
       menuVoices: null,
     };
   },
-  create() {
-    this.menuVoices = require('../menuLinks.json');
+  created() {
+    // eslint-disable-next-line global-require
+    this.menuVoices = require('../assets/menu-links.json');
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "../assets/scss/partials/_variables.scss";
 
+.ms_header-top {
+  font-size: 0.8em;
+
+  & a {
+    text-decoration: none;
+    color: $gull-gray;
+    text-transform: capitalize;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faTwitter, faInstagram, faBehance, faDribbble, faFlickr, faGit, faLinkedin, faPinterest,
+} from '@fortawesome/free-brands-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 
+library.add(faTwitter, faInstagram, faBehance, faDribbble,
+  faFlickr, faGit, faLinkedin, faPinterest, faChevronDown);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
